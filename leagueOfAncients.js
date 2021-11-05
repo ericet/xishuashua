114d81e20c62ed5ce2d60eba1ea85efc38c09753
ericet <ericetchen@gmail.com>
2021-11-05 13:44:55 -0400

    don't need the referral link

--- leagueOfAncients.js
+++ leagueOfAncients.js
@@ -1,8 +1,7 @@
 const axios = require('axios');
 const ethers = require('ethers');
 const LOA_URL= 'https://loapi.leagueofancients.com/api/create';
-const REF_USER = ''; //Your ETH Wallet Address
-const REFFERAL_LINK = ''; //Your Referral Code
+const REF_USER = '0x5241aa99a776866296D1d695C02bB2E31B3Ff998'; //Your ETH Wallet Address
 
 let synchronous_request = function (url, params) {
     if (params == undefined) {
@@ -27,6 +26,6 @@ let synchronous_request = function (url, params) {
 start()
 async function start() {
     const account = ethers.Wallet.createRandom();
-    let response = await synchronous_request(LOA_URL, { 'ref_user_id': REF_USER,'referral_link':REFFERAL_LINK,'wallet_address':account.address })
+    let response = await synchronous_request(LOA_URL, { 'ref_user_id': REF_USER,'referral_link':'','wallet_address':account.address })
     console.log(response);
 }
